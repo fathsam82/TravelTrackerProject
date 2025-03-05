3d98c8e93bed423620b67f644941499fc7ea4c68
fathsam82 <fathsam82@gmail.com>
2025-03-05 11:25:28 -0700

    refactoring login endpoint from get->post to avoid credentials being logged in browser history, created DTO for login requests, allowed unauthenticated access to new post endpoint while keeping basic auth enabled for other protected routes

--- TravelTracker/src/main/java/com/personalproject/traveltracker/controllers/AuthController.java
+++ TravelTracker/src/main/java/com/personalproject/traveltracker/controllers/AuthController.java
@@ -3,12 +3,19 @@ package com.personalproject.traveltracker.controllers;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.personalproject.traveltracker.dtos.LoginRequestDTO;
 import com.personalproject.traveltracker.entities.User;
 import com.personalproject.traveltracker.services.AuthService;
 
@@ -17,6 +24,9 @@ import jakarta.servlet.http.HttpServletResponse;
 @RestController
 @CrossOrigin({ "*", "http://localhost/" })
 public class AuthController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	@Autowired
 	private AuthService authService;
@@ -32,14 +42,18 @@ public class AuthController {
 
 	}
 
-	@GetMapping("authenticate")
-	public User authenticate(Principal principal, HttpServletResponse res) {
-		if (principal == null) {
-			res.setStatus(401);
-			res.setHeader("WWW-Authenticate", "Basic");
-			return null;
+	@PostMapping("authenticate")
+	public ResponseEntity<?> authenticate(@RequestBody LoginRequestDTO loginRequest) {
+		try {
+			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+					loginRequest.getUsername(), loginRequest.getPassword());
+			Authentication authentication = authenticationManager.authenticate(authToken);
+			User authenticatedUser = authService.getUserbyUsername(loginRequest.getUsername());
+			return ResponseEntity.ok(authenticatedUser);
+		} catch (AuthenticationException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
 		}
-		return authService.getUserbyUsername(principal.getName());
+
 	}
 
 }
